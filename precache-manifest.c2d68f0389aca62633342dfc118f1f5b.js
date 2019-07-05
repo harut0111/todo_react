240799a9f53a4fe43c778912d298cc4056c7a4c1
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-05 10:11:39 +0400

    Updates

---
+++ precache-manifest.c2d68f0389aca62633342dfc118f1f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cc08fcecf26ab159eaa1e8edbed068",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "c72241b6b36acf187bd9",
+    "url": "/todo_react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todo_react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c72241b6b36acf187bd9",
+    "url": "/todo_react/static/js/main.ed0e6314.chunk.js"
+  },
+  {
+    "revision": "8ae4c28430c1dff67705",
+    "url": "/todo_react/static/js/runtime~main.8f972959.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
