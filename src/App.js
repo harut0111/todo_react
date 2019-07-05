3bec29144984bf5b457f249ff3465e98896caff0
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-05 10:41:02 +0400

    added new message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Բարև Դավիթ, ո՞նց ես
+          Hello Davit, How are you? <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
