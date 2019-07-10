30421d3576c0d3511a841cd9336d481edd87dfdd
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-10 13:13:51 +0400

    to do list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Hello World !!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>To Do List</h1>
+      <input type="text"></input>
+      <button>add to do</button>
+      <div className="todoList">
+        asdfad
+        
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
