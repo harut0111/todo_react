92de0da8096a8ad34f4279b558c6a32453d173f8
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-16 17:57:35 +0400

    added firebase for list items

--- src/App.js
+++ src/App.js
@@ -11,49 +11,128 @@ class App extends React.Component{
     this.state = {
       input: "",
       todolist: [
-        {id: Math.random(), value: "I'm gonna do...", done: false}, 
-        {id: Math.random(), value: "I've already done it", done: true}
+        /* {id: Math.random(), value: "I'm gonna do...", done: false}, 
+        {id: Math.random(), value: "I've already done it", done: true} */
       ]
     };
   }
 
-  // store input value
+
+
+  /* ---- STORE INPUT VALUE ON STATE ---- */
   imputChangeHandler = (value) => {
       this.setState({input: value})
   }
 
-  //add new Items 
-  addTodoItems = () => {
-    const {input} = this.state;
-
-    this.setState(state => ({
-      todolist: state.todolist.concat({id: Math.random(), value: input, done: false}),
-      input: ""
-    }));
-  }
-
-
-  // remove item from list 
-  itemRomoveHandler = (key) => {
-    console.log(key)
-    const newList = this.state.todolist.filter(item => {
-      return item.id !== +key;
-    });  
-    console.log(newList)
-    this.setState({todolist: newList});
-  }
-
-  // mark item as done
-  itemDoneHandler = (key) => {
-    const newList = [...this.state.todolist]
-    newList.forEach(item => {
-      if(item.id === +key) item.done = !item.done
-    })
-
-    this.setState({todolist: newList});
-  }
 
   
+  /* ---- ADD NEW ITEMS ON LIST ---- */
+  addTodoItems = () => {
+
+    const {input} = this.state;
+    const randomNum = String(Math.random());
+
+    // Add a new document in collection "listItem" on firebase
+    this.db.collection("listItem").doc(randomNum ).set({
+      value: input, 
+      done: false
+    })
+    .then(() => { 
+      //here we sure that data has been set on firebase
+      this.setState(state => ({
+        todolist: state.todolist.concat({
+          id: randomNum, 
+          value: input, 
+          done: false
+        }),
+        input: ""
+      }));
+      /////
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });
+    
+
+  }
+
+
+  /* ---REMOVE ITEM FROM LIST--- */
+  itemRomoveHandler = (key) => {
+
+    //filter removed item
+    const newList = this.state.todolist.filter(item => {
+      return item.id !== key;
+    }); 
+
+    //delete from firebase then change "todolist" state
+    this.db.collection("listItem").doc(key).delete().then(() => {
+      this.setState({todolist: newList});
+    }).catch(function(error) {
+      console.error("Error removing document: ", error);
+    });
+  }
+
+
+
+  /* --- MARK ITEM AS DONE ---- */
+  itemDoneHandler = (key) => {
+    
+    const newList = [...this.state.todolist]
+    let itemDone = false;
+    newList.forEach(item => {
+      if(item.id === key) itemDone = item.done = !item.done
+    })
+
+    // at first change on firebase for sure then "todolist" state
+    this.db.collection("listItem").doc(key).set({
+      done: itemDone
+    },{ merge: true })
+    .then(() => {
+      this.setState({todolist: newList});
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });    
+  }
+
+
+  /* this method called after createing component
+  so it is the best place to initialize firebase */
+
+  componentDidMount() {
+    
+    const firebase = require("firebase");
+    // Required for side-effects
+    require("firebase/firestore");
+
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA-NlEgvbcRcsOT_ZP1j24kbZTKIsViHA4',
+      authDomain: 'todoapp-harut.firebaseapp.com',
+      projectId: 'todoapp-harut'
+    });
+    
+    this.db = firebase.firestore();
+
+    this.db.collection("listItem").get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+          this.setState(state => ({
+            todolist: state.todolist.concat({
+              id: doc.id, 
+              value: doc.data().value, 
+              done: doc.data().done
+            }),
+          }));
+          
+          /* console.log(`${doc.id} => ${doc.data().value} => ${doc.data().done}`); */
+         
+      });
+    });
+
+    
+  }
+
+
   render() {
     const {input, todolist} = this.state;
     return (
@@ -72,7 +151,8 @@ class App extends React.Component{
         <Todolist 
           todoItems={todolist} 
           onItemRemove={this.itemRomoveHandler}
-          onItemDone={this.itemDoneHandler}/>
+          onItemDone={this.itemDoneHandler}
+        />
       </div>
    )
   }
