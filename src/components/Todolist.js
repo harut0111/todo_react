5cf3ddf21de1bb53be5fd41b3afd2861f4eb2dcd
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-19 14:10:36 +0400

    rewrite with functional component

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import SimpleTooltips from './deleteTooltip';
 
-class Todolist extends React.Component {
+export default function Todolist(props) {
 
-    handleDoneAndRemove = (e) => {
-        
+    function handleDoneAndRemove(e) {
+
         e.target.tagName === "LI" ? (
-            this.props.onItemDone(e.target.id) 
-            ) : (
-            this.props.onItemRemove(e.currentTarget.id) );    
+        props.onItemDone(e.target.id) 
+        ) : (
+        props.onItemRemove(e.currentTarget.id) );    
     }
     
-    render() {
-        const { todoItems } = this.props;
-   
-        const items = todoItems.map(item => {
-            
-            const classType = item.done ? "done" : "";
-            return (
-                <li className={classType} 
-                    key={item.id}
-                    id={item.id}
-                    onClick={this.handleDoneAndRemove}
-                >
-                    {item.value} <SimpleTooltips onClick={this.handleDoneAndRemove}/>{/* <span >X</span> */} 
-                </li>
-            ) 
-        })
+    const { todoItems } = props;
 
+    const items = todoItems.map(item => {
+        
+        const classType = item.done ? "done" : "";
         return (
-            <ul>  
-                {items}
-            </ul>
-        )
-    }
-}
+            <li className={classType} 
+                key={item.id}
+                id={item.id}
+                onClick={handleDoneAndRemove}
+            >
+                {item.value} <SimpleTooltips onClick={handleDoneAndRemove}/>{/* <span >X</span> */} 
+            </li>
+        ) 
+    })
 
-export default Todolist;
+    return (
+        <ul>  
+            {items}
+        </ul>
+    )
+}
